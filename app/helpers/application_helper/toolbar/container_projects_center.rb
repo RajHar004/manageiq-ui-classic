58964d7ab56a1875035d3f65b0b4db04143da24b
RajHar004 <92999305+RajHar004@users.noreply.github.com>
2025-07-25 02:14:10 +0000

    container_projects_center.rb

--- app/helpers/application_helper/toolbar/container_projects_center.rb
+++ app/helpers/application_helper/toolbar/container_projects_center.rb
@@ -1,4 +1,37 @@
 class ApplicationHelper::Toolbar::ContainerProjectsCenter < ApplicationHelper::Toolbar::Basic
+  button_group('container_project_select', [
+    select(
+      :container_project_select_choice,
+      nil,
+      t = N_('Configuration'),
+      t,
+      :items => [
+        button(
+          :container_project_new,
+          'pficon pficon-add-circle-o fa-lg',
+          N_('Create New Project'),
+          N_('Create New Project'),
+          :url            => '/container_project/new',
+          :klass          => ApplicationHelper::Button::GenericFeatureButtonWithDisable,
+          :options        => { :feature => 'container_project_new' }
+        ),
+        button(
+          :container_project_delete,
+          'pficon pficon-delete fa-lg',
+          N_('Delete Selected Container Project'),
+          N_('Delete Container Project'),
+          :url_parms      => "main_div",
+          :send_checked   => true,
+          :confirm        => N_("Warning: The selected project will be permanently deleted!"),
+          :enabled        => false,
+          :onwhen         => "1+",
+          :klass          => ApplicationHelper::Button::GenericFeatureButtonWithDisable,
+          :options        => { :feature => 'container_project_delete' }
+        )
+      ]
+    )
+  ])
+
   button_group('container_project_policy', [
     select(
       :container_project_policy_choice,
@@ -7,17 +40,19 @@ class ApplicationHelper::Toolbar::ContainerProjectsCenter < ApplicationHelper::T
       t,
       :enabled => false,
       :onwhen  => "1+",
-      :items   => [
+      :items => [
         button(
           :container_project_tag,
           'pficon pficon-edit fa-lg',
-          N_('Edit Tags for this Service'),
+          N_('Edit Tags for this Project'),
           N_('Edit Tags'),
           :url_parms    => "main_div",
           :send_checked => true,
           :enabled      => false,
-          :onwhen       => "1+"),
+          :onwhen       => "1+"
+        )
       ]
-    ),
+    )
   ])
 end
+
