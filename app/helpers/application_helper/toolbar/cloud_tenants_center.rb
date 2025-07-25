928adb50abaf2f96b818be108836b68c62cf1037
RajHar004 <92999305+RajHar004@users.noreply.github.com>
2025-07-25 02:20:55 +0000

    cloud_tenants_center.rb

--- app/helpers/application_helper/toolbar/cloud_tenants_center.rb
+++ app/helpers/application_helper/toolbar/cloud_tenants_center.rb
@@ -1,4 +1,4 @@
-class ApplicationHelper::Toolbar::CloudTenantsCenter < ApplicationHelper::Toolbar::Basic
+class ApplicationHelper::Toolbar::CloudTenantCenter < ApplicationHelper::Toolbar::Basic
   button_group('cloud_tenant_vmdb', [
     select(
       :cloud_tenant_vmdb_choice,
@@ -7,52 +7,60 @@ class ApplicationHelper::Toolbar::CloudTenantsCenter < ApplicationHelper::Toolba
       t,
       :items => [
         button(
-          :cloud_tenant_new,
-          'pficon pficon-add-circle-o fa-lg',
-          t = N_('Create Cloud Tenant'),
-          t,
-          :klass => ApplicationHelper::Button::NewCloudTenant),
-        button(
           :cloud_tenant_edit,
           'pficon pficon-edit fa-lg',
-          N_('Select a single Cloud Tenant to edit'),
-          N_('Edit Selected Cloud Tenant'),
-          :url_parms    => "main_div",
-          :send_checked => true,
-          :enabled      => false,
-          :onwhen       => "1"),
+          t = N_('Edit this Cloud Tenant'),
+          t,
+          :klass => ApplicationHelper::Button::CloudTenantEdit
+        ),
         button(
           :cloud_tenant_delete,
           'pficon pficon-delete fa-lg',
-          N_('Delete selected Cloud Tenants'),
-          N_('Delete Cloud Tenants'),
-          :url_parms    => "main_div",
-          :send_checked => true,
-          :confirm      => N_("Warning: The selected Cloud Tenants will be permanently deleted!"),
-          :enabled      => false,
-          :onwhen       => "1+"),
+          t = N_('Remove this Cloud Tenant from Inventory'),
+          t,
+          :url_parms => "&refresh=y",
+          :confirm   => N_("Warning: This Cloud Tenant and ALL of its components will be permanently removed!"),
+          :klass     => ApplicationHelper::Button::CloudTenantDelete
+        )
       ]
     ),
   ])
+
   button_group('cloud_tenant_policy', [
     select(
       :cloud_tenant_policy_choice,
       nil,
       t = N_('Policy'),
       t,
-      :enabled => false,
-      :onwhen  => "1+",
-      :items   => [
+      :items => [
         button(
           :cloud_tenant_tag,
           'pficon pficon-edit fa-lg',
-          N_('Edit tags for the selected items'),
+          N_('Edit Tags for this Cloud Tenant'),
           N_('Edit Tags'),
-          :url_parms    => "main_div",
-          :send_checked => true,
-          :enabled      => false,
-          :onwhen       => "1+"),
+          :klass => ApplicationHelper::Button::CloudTenantTag
+        ),
       ]
+    ),
+  ])
+
+  button_group('ems_cloud', [
+    twostate(
+      :view_dashboard,
+      'fa fa-tachometer fa-1xplus',
+      N_('Dashboard View'),
+      nil,
+      :url       => "/show/",
+      :url_parms => "?display=dashboard",
+      :klass     => ApplicationHelper::Button::ViewDashboard
+    ),
+    twostate(
+      :view_summary,
+      'fa fa-th-list',
+      N_('Summary View'),
+      nil,
+      :url       => "/show/",
+      :url_parms => "?display=main"
     ),
   ])
 end
